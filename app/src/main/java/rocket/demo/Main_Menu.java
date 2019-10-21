d23aaab10672544b7b49adcf39a5bfc2b21b8d04
marcello san agustin <marcello.sanagustin.448@my.csun.edu>
2019-10-21 16:27:22 -0700

    Sprint 3
    login signup main menu done, needs to add database

---
+++ app/src/main/java/rocket/demo/Main_Menu.java
@@ -0,0 +1,12 @@
+package rocket.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main_Menu extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainmenu_activity);
+    }
+}
